bf9fa685e4fe389fb4508d81a7bc4af2021417bf
bjossi86 <bjorn.vignir@gmail.com>
2025-03-17 10:29:42 +0000

    feat: service worker to fetch new assets

---
+++ client/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'my-cache-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/branding.css',
+        '/assets/logo.svg',
+        '/assets/favicon-12x12.png',
+        '/assets/favicon-32x32.png',
+        '/assets/apple-touch-icon-180x180.png',
+      ]);
+    }),
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        }),
+      );
+    }),
+  );
+});
+
+// Athugar hvort ný útgáfa sé til
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => caches.match(event.request)),
+  );
+});
